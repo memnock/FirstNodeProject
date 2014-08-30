6392c7ff7e53f73c92338998ed74ddb32bdd1076
jeremylassiter <jeremylassiter@gmail.com>
2014-08-29 21:16:46 -0400

    Before Heroku

---
+++ server.js
@@ -0,0 +1,107 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+if(env === 'development') {
+    mongoose.connect('mongodb://localhost/multivision');
+}
+else {
+    mongoose.connect('mongodb://jlassiter:multivision@ds045077.mongolab.com:45077/multivision')
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    if(messageDoc != null) {
+        mongoMessage = messageDoc.message;
+    }
+    else
+    {
+        mongoMessage = "No messages";
+    }
+    mongoMessage = messageDoc.message;
+});
+
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
